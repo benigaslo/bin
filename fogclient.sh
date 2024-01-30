28d6576527eda20eb7630d172674a2dc096bc88d
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-01-30 10:01:37 +0100

    Update fogclient.sh

--- fogclient.sh
+++ fogclient.sh
@@ -1,3 +1,16 @@
+HOSTNAME=$1
+while [[ -z $HOSTNAME ]] 
+do
+    echo "HOSTNAME:"
+    read HOSTNAME
+done
+
+echo "$HOSTNAME" > /etc/hostname
+
+sed -i "/127.0.1.1/d" /etc/hosts
+printf "127.0.1.1\t$HOSTNAME" >> /etc/hosts 
+
+
 apt update
 apt -y install dirmngr gnupg apt-transport-https ca-certificates software-properties-common wget
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF

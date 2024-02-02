f3842c791533c9ea6a5eb573bab9eb17329343f5
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-02-02 12:04:58 +0100

    Create monitors_spread.sh

---
+++ monitors_spread.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copia l'arxiu monitors.xml en les carpetes dels usuaris
+for d in /home/*/; do
+  mkdir -p $d/.config;
+  cp /var/lib/gdm3/.config/monitors.xml $d/.config/monitors.xml;
+  chown --reference=$d $d/.config/monitors.xml;
+done

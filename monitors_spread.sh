e17309c5a71cfb7e8a249822b4ade3282bd50275
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-02-06 09:23:29 +0100

    Update monitors_spread.sh

--- monitors_spread.sh
+++ monitors_spread.sh
@@ -6,3 +6,5 @@ for d in /home/*/; do
   cp /var/lib/gdm3/.config/monitors.xml $d/.config/monitors.xml;
   chown --reference=$d $d/.config/monitors.xml;
 done
+
+# for d in /home/*/; do mkdir -p $d/.config; cp /var/lib/gdm3/.config/monitors.xml $d/.config/monitors.xml; chown --reference=$d $d/.config/monitors.xml; done

67aa9edf01d0f94e2eee13dddfb1bab009de9b6d
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-10-11 09:24:46 +0200

    Update capaor_install.sh

--- capaor_install.sh
+++ capaor_install.sh
@@ -1,25 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $1 == "up" ]; then
+echo "Capant internet..."
+echo "  Els alumnes han de configurar el proxy (o servidor intermediari) al Firefox:"
+echo "    * Proxy HTTP: 10.2.1.254   |  Port: 3128"
+echo "      [v] Usar tambien para HTTPS"
 apt update
 apt install squid
-
 cat << EOF > /etc/squid/conf.d/capaor.conf
 acl PERMES dstdomain .gva.es
 http_access allow PERMES
 EOF
-
-cat << EOF > /usr/local/sbin/capaor
-#!/usr/bin/env bash
-
-if [ $1 == "up" ]; then
-        echo "Capant internet..."
-        echo "Els alumnes han de configurar el proxy (o servidor intermediari) al Firefox:"
-        echo "  Proxy HTTP: 10.2.1.254   |  Port: 3128"
-        echo "  [v] Usar tambien para HTTPS"
-        iptables -P FORWARD DROP
-        iptables -D FORWARD -j ACCEPT 2> /dev/null
+systemctl restart squid
+iptables -P FORWARD DROP
+iptables -D FORWARD -j ACCEPT 2> /dev/null
 elif [ $1 == "down" ]; then
-        echo "Habilitant internet..."
-        iptables -P FORWARD ACCEPT
-fi
+echo "Habilitant internet..."
+echo "  Els alumnes ja poden deshabilitar el proxy (no es necessari)"
+cat << EOF > /etc/squid/conf.d/capaor.conf
+http_access allow all
 EOF
-
-chmod +x /usr/local/sbin/capaor
+systemctl restart squid
+iptables -P FORWARD ACCEPT
+fi
